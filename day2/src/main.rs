dad38f1f7c7429057656e70cf77e5c35ca0d6398
Will Kahn-Greene <willkg@mozilla.com>
2020-09-01 11:33:30 -0400

    Day 2: cargo fmt and cargo clippy pass

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,12 +7,12 @@ use std::io::prelude::*;
 use std::path::Path;
 
 /// Run the given program and return the output
-fn run_program(data : &Vec<usize>) -> Vec<usize> {
+fn run_program(data: &Vec<usize>) -> Vec<usize> {
     // Copy the vector
     let mut data_out = data.to_vec();
 
-    let end_i : usize = data_out.len();
-    let mut i : usize = 0;
+    let end_i: usize = data_out.len();
+    let mut i: usize = 0;
     loop {
         if i >= end_i {
             break;
@@ -21,26 +21,26 @@ fn run_program(data : &Vec<usize>) -> Vec<usize> {
         let opcode = data_out[i];
         match opcode {
             1 => {
-                let lhs_i = data_out[i+1];
-                let rhs_i = data_out[i+2];
-                let dest_i = data_out[i+3];
+                let lhs_i = data_out[i + 1];
+                let rhs_i = data_out[i + 2];
+                let dest_i = data_out[i + 3];
                 data_out[dest_i] = data_out[lhs_i] + data_out[rhs_i];
-                i = i + 4;
-            },
+                i += 4;
+            }
             2 => {
-                let lhs_i = data_out[i+1];
-                let rhs_i = data_out[i+2];
-                let dest_i = data_out[i+3];
+                let lhs_i = data_out[i + 1];
+                let rhs_i = data_out[i + 2];
+                let dest_i = data_out[i + 3];
                 data_out[dest_i] = data_out[lhs_i] * data_out[rhs_i];
-                i = i + 4;
-            },
+                i += 4;
+            }
             99 => {
                 break;
-            },
+            }
             _ => {
                 eprintln!("Unknown operand: {}", data_out[i]);
                 std::process::exit(exitcode::DATAERR);
-            },
+            }
         }
     }
 
@@ -54,19 +54,13 @@ mod tests {
     /// Test 1 oper
     #[test]
     fn test_run_program_oper_1() {
-        assert_eq!(
-            run_program(&vec![1, 0, 0, 0, 99]),
-            vec![2, 0, 0, 0, 99]
-        );
+        assert_eq!(run_program(&vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
     }
 
     /// Test 2 oper
     #[test]
     fn test_run_program_oper_2() {
-        assert_eq!(
-            run_program(&vec![2, 3, 0, 3, 99]),
-            vec![2, 3, 0, 6, 99]
-        );
+        assert_eq!(run_program(&vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
     }
 
     /// Test complex
@@ -93,7 +87,7 @@ fn main() {
         Err(e) => {
             eprintln!("Can't open file: {}", e);
             std::process::exit(exitcode::DATAERR);
-        },
+        }
         Ok(file) => file,
     };
 
@@ -103,7 +97,7 @@ fn main() {
         Err(e) => {
             eprintln!("Can't read file: {}", e);
             std::process::exit(exitcode::DATAERR);
-        },
+        }
     };
 
     // Remove whitespace from beginning and end
@@ -111,7 +105,10 @@ fn main() {
     println!("input: {}", contents);
 
     // Split the input on "," and convert to usize
-    let mut prog_input: Vec<usize> = contents.split(",").map(|val| usize::from_str_radix(val, 10).unwrap()).collect();
+    let mut prog_input: Vec<usize> = contents
+        .split(',')
+        .map(|val| usize::from_str_radix(val, 10).unwrap())
+        .collect();
 
     'outer: for input_noun in 0..99 {
         for input_verb in 0..99 {
